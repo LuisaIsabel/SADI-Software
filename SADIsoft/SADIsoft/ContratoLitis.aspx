b2c4ad9f94880729a757068859ad974ffa8bce8c
franceslugo18 <francisco.lugo@outlook.com>
2015-05-21 10:27:29 -0400

    mod

---
+++ SADIsoft/SADIsoft/ContratoLitis.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ContratoLitis.aspx.cs" Inherits="SADIsoft.ContratoLitis" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        Seleccione propietario:&nbsp;&nbsp;&nbsp; <asp:DropDownList ID="ddlPropietario1" runat="server" AutoPostBack="True"></asp:DropDownList>
+        <br /><br /><br />
+        <asp:Button ID="Button1" runat="server" Text="Generar Contrato" />
+    </div>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server"></asp:SqlDataSource>
+    </form>
+</body>
+</html>
---
+++ SADIsoft/SADIsoft/ContratoLitis.aspx.cs
@@ -0,0 +1,31 @@
+﻿using SADIsoft.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SADIsoft
+{
+    public partial class ContratoLitis : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CargarPropietarios();            
+
+
+            }
+        }
+
+        private void CargarPropietarios()
+        {
+            ddlPropietario1.DataSource = PropietarioDA.CargarPropietariosDB();
+            ddlPropietario1.DataTextField = "Nombre";
+            ddlPropietario1.DataValueField = "PropietarioId";
+            ddlPropietario1.DataBind();
+        }
+    }
+}
